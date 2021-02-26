9a1d0454204afdd17567c8996c5deb4fd603bd9d
maradondt <maradondt@gmail.com>
2021-02-26 17:59:12 +0300

    Setting up the environment, less modules, stylelint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import * as routes from './routes';
+import './App.less';
+import Table from './components/Pagination/Table/Table';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loadingProcessState, setLoadingProcessState] = useState('loading');
+
+  useEffect(() => {
+    axios.get(routes.postsPath())
+      .then((response) => {
+        setData(response.data);
+        setLoadingProcessState('idle');
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoadingProcessState('error')
+      });
+  }, [loadingProcessState]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <Table
+          data={data}
+          countItems={50}
+        />
+      </main>
     </div>
   );
 }
